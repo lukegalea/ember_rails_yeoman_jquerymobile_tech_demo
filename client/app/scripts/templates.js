1f5f34b80f9c0c0d69d3b9f37052f50535834212
Luke Galea <galeal@Lukes-Mac-Pro.local>
2013-02-09 20:48:12 -0500

    Add bootstrap & font-awesome

--- client/app/scripts/templates.js
+++ client/app/scripts/templates.js
@@ -3,11 +3,11 @@ helpers = helpers || Ember.Handlebars.helpers; data = data || {};
   var buffer = '', stack1, hashTypes, escapeExpression=this.escapeExpression;
 
 
-  data.buffer.push("<header><h1>Foody</h1></header><div id=\"mainOutlet\">");
+  data.buffer.push("<header><h1>Foody</h1></header><div class=\"container\">");
   stack1 = {};
   hashTypes = {};
   stack1 = helpers._triageMustache.call(depth0, "outlet", {hash:stack1,contexts:[depth0],types:["ID"],hashTypes:hashTypes,data:data});
-  data.buffer.push(escapeExpression(stack1) + "</div><footer>&copy;2013 Luke Galea <luke<AT>ideaforge.org></footer>");
+  data.buffer.push(escapeExpression(stack1) + "</div><footer>&copy;2013 Luke Galea</footer>");
   return buffer;
 });
 
@@ -16,5 +16,5 @@ helpers = helpers || Ember.Handlebars.helpers; data = data || {};
   
 
 
-  data.buffer.push("<p> \nThis is the index</p>");
+  data.buffer.push("<i class=\"icon-play icon-large\"></i>");
 });
